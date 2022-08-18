aa81a9f79f5dabdcd320686f186403afb63dd7d3
Harold Rojas <harold.rojas.e@gmail.com>
2022-08-18 18:43:48 -0400

    Creating requirements and back to seaborn

--- data_viz.py
+++ data_viz.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('CSV Reader')
@@ -8,9 +9,24 @@ if file:
     df = pd.read_csv(file)
     st.dataframe(df)
     st.markdown("---")
-    fig1 = plt.figure(figsize =(10, 4))
-    plt.hist(df['Sex'])
+    fig1 = plt.figure(figsize=(10, 4))
+    sns.countplot(x='Pclass', data=df)
 
     st.pyplot(fig1)
 
     st.markdown("---")
+
+    fig2 = plt.figure(figsize=(10, 4))
+    sns.boxplot(x='Pclass',
+                y='Age',
+                data = df
+    )
+
+    st.pyplot(fig2)
+
+    st.markdown("---")
+    
+    fig3 = plt.figure(figsize=(10, 4))
+    sns.heatmap(df.corr(), annot=True)
+
+    st.pyplot(fig3)
