3e04c2fc9dfab6bfc0e0c3cd84e103890866dc5a
Harold Rojas <harold.rojas.e@gmail.com>
2022-08-18 18:24:18 -0400

    Taking out seaborn

--- data_viz.py
+++ data_viz.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('CSV Reader')
@@ -9,24 +8,9 @@ if file:
     df = pd.read_csv(file)
     st.dataframe(df)
     st.markdown("---")
-    fig1 = plt.figure(figsize=(10, 4))
-    sns.countplot(x='Pclass', data=df)
+    fig1 = plt.figure(figsize =(10, 4))
+    plt.hist(df['Sex'])
 
     st.pyplot(fig1)
 
     st.markdown("---")
-
-    fig2 = plt.figure(figsize=(10, 4))
-    sns.boxplot(x='Pclass',
-                y='Age',
-                data = df
-    )
-
-    st.pyplot(fig2)
-
-    st.markdown("---")
-    
-    fig3 = plt.figure(figsize=(10, 4))
-    sns.heatmap(df.corr(), annot=True)
-
-    st.pyplot(fig3)
